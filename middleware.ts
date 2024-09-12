ae0c9d33d799c43bda7f6f65347431e6a90bafc4
Sangam Neela <neelasangam8433@gmail.com>
2024-09-12 16:41:26 +0530

    reverting changes in middlware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,10 @@
 import {auth} from "@/auth"
 import { apiAuthPrefix,authRoutes,publicRoutes } from "./routes";
-import { getToken } from "next-auth/jwt";
-import { NextRequest } from "next/server";
 
-export default async function middleware(req:NextRequest){
+
+export default  auth((req)=>{
     const {nextUrl} = req;
-    // const isLoggedin= !!req.auth;
-    const secret = process.env.AUTH_SECRET as string;
-    const token = await getToken({ req, secret,salt:"100"});
-    const isLoggedin = !!token;
+    const isLoggedin= !!req.auth;
     console.log("ISLOGGES IN = ",isLoggedin);
     const isApiAuthRoute=nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute=publicRoutes.includes(nextUrl.pathname);
@@ -32,7 +28,7 @@ export default async function middleware(req:NextRequest){
         return Response.redirect(new URL("/auth/login",nextUrl));
     }
     return
-}
+})
 
 
 export const config={
