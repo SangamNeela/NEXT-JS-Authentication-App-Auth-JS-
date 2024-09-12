3090e70d3f475b1a1068bb7a2b4e8e20ff3fc5d6
Sangam Neela <neelasangam8433@gmail.com>
2024-09-12 16:15:02 +0530

    middleware fix 2

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { apiAuthPrefix,authRoutes,publicRoutes } from "./routes";
 
 export default  auth((req)=>{
     const {nextUrl} = req;
-    const isLoggedin= req.auth?.user
+    const isLoggedin= !!req.auth
     console.log("ISLOGGES IN = ",isLoggedin);
     const isApiAuthRoute=nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute=publicRoutes.includes(nextUrl.pathname);
