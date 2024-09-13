5c73a90dd66dad580d2c9ce93bad589a08892d84
Sangam Neela <neelasangam8433@gmail.com>
2024-09-13 12:21:39 +0530

    changes in auth.ts

--- auth.ts
+++ auth.ts
@@ -61,6 +61,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return true;
     },
     async session({session,token}){
+      console.log("{session from sess = }",session);
+      console.log("{token from session = }",token);
       if(token.sub && session.user){
         session.user.id=token.sub;
       }
@@ -82,6 +84,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
     async jwt({token,user}){
+      console.log("{jwt user = }",user);
+      console.log("{jwt token = }",token);
       if(user){
         token.name=user.name;
         token.role=user.role;
